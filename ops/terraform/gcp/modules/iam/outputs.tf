ce77d0cb70f597d6057da8d97d3cca73dd704bdb
cfegela <chris.fegela@oddball.io>
2026-01-27 17:59:58 -0500

    hard to troubleshoot cors in a new cloud. will circle back on gcp later

--- ops/terraform/gcp/modules/iam/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "api_service_account_email" {
-  description = "Service account email for Cloud Run API"
-  value       = google_service_account.api.email
-}
-
-output "api_service_account_id" {
-  description = "Service account ID for Cloud Run API"
-  value       = google_service_account.api.id
-}
